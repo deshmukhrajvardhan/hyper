ed18a9e4f8903075849980f04d0377f089afad05
RAJVARDHAN SOMRAJ DESHMUKH <17696886+deshmukhrajvardhan@users.noreply.github.com>
2018-01-29 14:35:43 -0500

    read_chunks_function

--- hyper/http20/response.py
+++ hyper/http20/response.py
@@ -188,6 +188,49 @@ time_after_read,time_after_decode,time_after_flush)+"\n")
         self.close()
 
         return
+## My Changes                                                                                       
+
+    def read_chunked_give_size(self, amt=None, decode_content=True):
+        """                                                                                         
+        Reads chunked transfer encoded bodies. This method returns a generator:                     
+        each iteration of which yields one chunk *unless* the frames                           
+        contain compressed data and ``decode_content`` is ``True``, in which                        
+        case it yields whatever the decompressor provides for each chunk.                           
+                                                                                                    
+        .. warning:: This may yield the empty string, without that being the                        
+                     end of the body!                                                               
+        """
+        chunk_start_time = timeit.default_timer()
+
+        while True:
+            data = self._stream._read(amt)
+
+            #time_after_read = timeit.default_timer() - chunk_start_time                            
+            if data is None:
+                break
+
+            if decode_content and self._decompressobj:
+                data = self._decompressobj.decompress(data)
+
+            #time_after_decode = timeit.default_timer() - chunk_start_time                          
+            yield data
+
+        time_after_decode = timeit.default_timer() - chunk_start_time
+
+        if decode_content and self._decompressobj:
+            yield self._decompressobj.flush()
+
+        time_after_flush = timeit.default_timer() - chunk_start_time
+
+        with open("/mnt/QUIClientServer0/gen-hyper-read-steps",'a') as gen_hyper_read_steps:
+            gen_hyper_read_steps.write("hyper-http2-read_chunk_start_time,{},{},{}\n".format(chunk_\
+start_time,time_after_decode,time_after_flush))
+
+        self.close()
+
+        return
+
+## \My Changes                                                                                      
 
     def fileno(self):
         """
